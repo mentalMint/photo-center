a6e5c6fb922912b52aab198690a29917b79cd115
mentalMint <walkershush@gmail.com>
2023-05-10 23:12:38 +0700

    feat: add triggers

--- Queries.sql
+++ Queries.sql
@@ -238,14 +238,26 @@ INSERT INTO "public.photo_shop_types" (id, type_name) VALUES
 	(1, 'shop'),
 	(2, 'kiosk');
 
-INSERT INTO "public.photo_shops" (type_id, address) VALUES
+INSERT INTO "public"."photo_shops" (type_id, address) VALUES
 	(1, 'Терешковой, 12'),
-	(2, 'Ильича, 6a');
+	(2, 'Ильича, 6a'),
+	(1, 'Ленина, 27'),
+	(2, 'Пролетарская, 3'),
+	(3, 'Советская, 10');
+
+
 
 INSERT INTO "public.clients" (first_name, last_name) VALUES
 	('John', 'Smith'),
 	('Abrik', 'Valishev'),
 	('Tony', 'Stark');
+
+INSERT INTO "public.clients" (first_name, last_name) VALUES
+	('Alice', 'Johnson'),
+	('Bob', 'Williams'),
+	('Carla', 'Garcia');
+
+
 
 INSERT INTO "public.workplaces" (workplace_name) VALUES
 	('Кассир'),
@@ -253,11 +265,25 @@ INSERT INTO "public.workplaces" (workplace_name) VALUES
 	('Администратор'),
 	('Фотограф');
 
+INSERT INTO "public.workplaces" (workplace_name) VALUES
+	('Менеджер по продажам'),
+	('Дизайнер'),
+	('Оператор сканирования'),
+	('Ретушер');
+
+
 INSERT INTO "public.firms" (firm_name) VALUES
 	('Canon'),
 	('Nikon'),
 	('Polaroid'),
 	('Sony');
+
+INSERT INTO public.firms (firm_name) VALUES
+	('Fujifilm'),
+	('Leica'),
+	('Panasonic'),
+	('Olympus');
+
 
 INSERT INTO "public.offer_types" (offer_type_name) VALUES
 	('Товар'),
@@ -690,3 +716,223 @@ FROM "public.workplaces" AS W
 	JOIN "public.photo_shops" AS PS
 		ON PS.id = PSW.photo_shop_id
 WHERE W.workplace_name = 'Кассир'
+
+
+CREATE OR REPLACE FUNCTION check_discount_range()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.discount >= 0 AND NEW.discount <= 100 THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'discount must be greater than 0 and less than or equal to 100';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER discount_clients_photo_shops
+BEFORE UPDATE ON "public.clients_photo_shops"
+FOR EACH ROW
+EXECUTE FUNCTION check_discount_range();
+
+
+CREATE OR REPLACE FUNCTION check_film_development_orders_cost()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.cost >= 0 THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Cost must be greater than or equal to 0';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER film_development_orders_cost_check
+BEFORE INSERT OR UPDATE ON "public.film_development_orders"
+FOR EACH ROW
+EXECUTE FUNCTION check_film_development_orders_cost();
+
+
+CREATE OR REPLACE FUNCTION check_print_orders_cost()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.cost >= 0 THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Cost must be greater than or equal to 0';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER print_orders_cost_check
+BEFORE INSERT OR UPDATE ON "public.print_orders"
+FOR EACH ROW
+EXECUTE FUNCTION check_print_orders_cost();
+
+
+CREATE OR REPLACE FUNCTION check_offers_cost()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.cost >= 0 THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Cost must be greater than or equal to 0';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER offers_cost_check
+BEFORE INSERT OR UPDATE ON "public.offers"
+FOR EACH ROW
+EXECUTE FUNCTION check_offers_cost();
+
+
+CREATE OR REPLACE FUNCTION check_photo_shops_workplaces_number()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.number > 0 THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Number must be greater than 0';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER photo_shops_workplaces_number_check
+BEFORE INSERT OR UPDATE ON "public.photo_shops_workplaces"
+FOR EACH ROW
+EXECUTE FUNCTION check_photo_shops_workplaces_number();
+
+
+CREATE OR REPLACE FUNCTION check_supply_requests_number_of_products()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.number_of_products > 0 THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Number of products must be greater than 0';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER supply_requests_number_of_products_check
+BEFORE INSERT OR UPDATE ON "public.supply_requests"
+FOR EACH ROW
+EXECUTE FUNCTION check_supply_requests_number_of_products();
+
+
+
+CREATE OR REPLACE FUNCTION check_print_orders_numbers()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.photos_number > 0 AND NEW.copies_number > 0 THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Photos number and copies number must be greater than 0';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER print_orders_numbers_check
+BEFORE INSERT OR UPDATE ON "public.print_orders"
+FOR EACH ROW
+EXECUTE FUNCTION check_print_orders_numbers();
+
+
+CREATE OR REPLACE FUNCTION check_clients_photo_shops_discount_card_number()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM public.clients_photo_shops
+        WHERE discount_card_number = NEW.discount_card_number
+    ) THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Discount card number already exists';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER clients_photo_shops_discount_card_number_check
+BEFORE INSERT OR UPDATE ON "public.clients_photo_shops"
+FOR EACH ROW
+EXECUTE FUNCTION check_clients_photo_shops_discount_card_number();
+
+
+CREATE OR REPLACE FUNCTION check_orders_print_or_film_id_not_null()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.print_order_id IS NOT NULL OR NEW.film_development_id IS NOT NULL THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Print order ID or film development ID must not be null';
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "orders_print_or_film_id_check"
+BEFORE INSERT OR UPDATE ON "public.orders"
+FOR EACH ROW
+EXECUTE FUNCTION check_orders_print_or_film_id_not_null();
+
+
+CREATE OR REPLACE FUNCTION convert_firm_name_to_uppercase()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.firm_name := UPPER(NEW.firm_name);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "convert_firm_name_to_uppercase_trigger"
+BEFORE INSERT OR UPDATE ON "public.firms"
+FOR EACH ROW
+EXECUTE FUNCTION convert_firm_name_to_uppercase();
+
+
+
+CREATE OR REPLACE FUNCTION check_discount_when_card_number_not_null()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.discount_card_number IS NOT NULL AND NEW.discount IS NULL THEN
+        RAISE EXCEPTION 'Discount cannot be null when discount card number is not null.';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "check_discount_card_number_not_null_trigger"
+BEFORE INSERT OR UPDATE ON "public.clients_photo_shops"
+FOR EACH ROW
+EXECUTE FUNCTION check_discount_when_card_number_not_null();
+
+
+CREATE OR REPLACE FUNCTION check_date_not_greater_than_now()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.date > NOW() THEN
+        RAISE EXCEPTION 'Date cannot be in the future.';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "check_date_not_greater_than_now_trigger"
+BEFORE INSERT OR UPDATE ON "public.orders"
+FOR EACH ROW
+EXECUTE FUNCTION check_date_not_greater_than_now();
+
+
+CREATE OR REPLACE FUNCTION check_date_not_greater_than_now()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.date > NOW() THEN
+        RAISE EXCEPTION 'Date cannot be in the future.';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER "check_date_not_greater_than_now_trigger"
+BEFORE INSERT OR UPDATE ON "public.offers"
+FOR EACH ROW
+EXECUTE FUNCTION check_date_not_greater_than_now();
